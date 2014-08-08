3e09585da5ad2badae0a197c21464ff0f765cf0e
Daniel Fekete <danieleff@gmail.com>
2014-08-08 22:52:47 +0200

    init

---
+++ desktop/src/openrestaurant/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package openrestaurant.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import openrestaurant.OpenRestaurantGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OpenRestaurantGame(), config);
+	}
+}
